d296c68a788b4c863520371d994441133cd611ec
J-Christophe Lejeune <38323623+jclejeune@users.noreply.github.com>
2023-10-31 14:11:31 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+
+<body style="background-color:grey">
+    <div class="m-4 p-8" style="background-color: white">
+        <h1 class="uppercase text-center">Bienvenue sur mon blog</h1>
+        <p>
+            <a href="{{ url('/posts') }}" class="uppercase text-center">Visiter Mon Blog</a>
+        </p>
+    </div>
+
+
+</body>
+
+</html>
